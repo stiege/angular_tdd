55af2eaeab8a0d7a18d764547772b6010c30a70f
Alex <alex.hodge@gmail.com>
2016-10-15 20:57:09 +1300

    Add d3js scheduler

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 // Define the `app` module
 var app = angular.module('app', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/');
+    $stateProvider.state('home', {
+        url: '/',
+        templateUrl: 'includes/graph.html'
+    });
+});
